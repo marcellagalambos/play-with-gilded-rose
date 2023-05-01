1a92051fb302cd7c6c8ab5cb73311341c1409172
Marcella Galambos <galambos.marcella@gmail.com>
2023-05-01 17:40:44 +0200

    Added tests for Sulfuras and Backstage passes - advanced

--- backend/Advanced/src/Test/GildedRoseTest.php
+++ backend/Advanced/src/Test/GildedRoseTest.php
@@ -11,6 +11,8 @@ class GildedRoseTest extends TestCase
      * @test
      * @dataProvider breadItems
      * @dataProvider brieItems
+     * @dataProvider sulfurasItems
+     * @dataProvider backstagePassItems
      * @dataProvider manaDrinkItems
      */
     public function it_updates_the_item(GildedRose $item, int $expectedQuality, int $expectedSellIn): void
@@ -73,6 +75,53 @@ class GildedRoseTest extends TestCase
             'it decreases sell-in even after the sell-in date has been reached and quality is at maximum for Aged Brie' => [
                 'item' => GildedRose::of('Aged Brie', 50, 0),
                 'expectedQuality' => 50,
+                'expectedSellIn' => -1,
+            ],
+        ];
+    }
+
+    public static function sulfurasItems(): array
+    {
+        return [
+            'it does not decrease the sell-in and does not change the quality for Sulfuras' => [
+                'item' => GildedRose::of('Sulfuras, Hand of Ragnaros', 80, 5),
+                'expectedQuality' => 80,
+                'expectedSellIn' => 5,
+            ],
+        ];
+    }
+
+    public static function backstagePassItems(): array
+    {
+        return [
+            'it decreases the sell-in but increases the quality before sell-in date for Backstage passes' => [
+                'item' => GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 10, 12),
+                'expectedQuality' => 11,
+                'expectedSellIn' => 11,
+            ],
+            'it increases quality twice as fast when sell-in date is 10 days or less for Backstage passes' => [
+                'item' => GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 10, 10),
+                'expectedQuality' => 12,
+                'expectedSellIn' => 9,
+            ],
+            'it increases quality twice as fast when sell-in date is 5 days or less for Backstage passes' => [
+                'item' => GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 10, 5),
+                'expectedQuality' => 13,
+                'expectedSellIn' => 4,
+            ],
+            'it does not increase quality past 50 for Backstage passes' => [
+                'item' => GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 50, 5),
+                'expectedQuality' => 50,
+                'expectedSellIn' => 4,
+            ],
+            'it does not increase quality past 50 even when a tick would take it over 50 for Backstage passes' => [
+                'item' => GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 49, 2),
+                'expectedQuality' => 50,
+                'expectedSellIn' => 1,
+            ],
+            'it decreases sell-in even after the sell-in date has been reached and decreases quality to 0 for Backstage passes' => [
+                'item' => GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 50, 0),
+                'expectedQuality' => 0,
                 'expectedSellIn' => -1,
             ],
         ];
